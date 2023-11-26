0b3852bcce2aed073301d9d007550c1582efe552
k33g <ph.charriere@gmail.com>
2023-11-26 05:51:19 +0000

    🌸 Gardening.

--- samples/flock/basestar/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-simplism listen \
-basestar.wasm handle --http-port 8080 --log-level info
-
--- samples/flock/queries.sh
+++ samples/flock/queries.sh
@@ -8,8 +8,3 @@ do
 
     curl http://localhost:9000/raiders
 done
-
-
-#hey -n 3000 -c 1000 -m GET http://localhost:8001/move &
-#hey -n 3000 -c 1000 -m GET http://localhost:8002/move &
-#hey -n 3000 -c 1000 -m GET http://localhost:8003/move &
