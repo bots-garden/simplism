d0fe0c94b705e10e04f46fe1d2171296ec1409d2
Philippe Charrière <admin@bots.garden>
2023-12-05 07:27:02 +0000

    📝 Add or update documentation: check the config mode

--- samples/golang/hello-plugin/run-with-config.sh
+++ samples/golang/hello-plugin/run-with-config.sh
@@ -2,5 +2,10 @@
 clear
 bat $0 --line-range 5:
 echo ""
-go run ../../../main.go config ./config.yml hello-plugin
+go run ../../../main.go config ./config.yml hello-plugin &
+
+#go run ../../../main.go config ./config.yml hello-plugin-1 &
+
+#go run ../../../main.go config ./config.yml hello-plugin-2 &
+
 
