5d5c01a4cc7ab14e36296420254daa27cafe52ac
Philippe Charrière <ph.charriere@gmail.com>
2023-12-17 21:05:04 +0000

    💾 Saved.

--- docker-4-gitpod/build-push.sh
+++ docker-4-gitpod/build-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGE_BASE_NAME="gitpod-simplism-playground"
-IMAGE_TAG="0.0.1"
+IMAGE_TAG="0.0.2"
 docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
 docker buildx build \
 --build-arg="GO_ARCH=amd64" \
@@ -9,7 +9,7 @@ docker buildx build \
 --build-arg="TINYGO_VERSION=0.30.0" \
 --build-arg="EXTISM_VERSION=0.3.3" \
 --build-arg="EXTISM_ARCH=amd64" \
---build-arg="EXTISM_JS_VERSION=1.0.0-rc1" \
+--build-arg="EXTISM_JS_VERSION=1.0.0-rc3" \
 --build-arg="EXTISM_JS_ARCH=x86_64" \
 --build-arg="EXTISM_JS_OS=linux" \
 --build-arg="NODE_MAJOR=20" \
