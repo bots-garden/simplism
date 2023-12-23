dae3e7ef643da0b57fd4e2008894c94aa266279c
Philippe Charrière <ph.charriere@gmail.com>
2023-12-23 12:30:25 +0000

    🛟 Updated: docker image for Gitpod 🍊

--- docker-4-gitpod/build-push.sh
+++ docker-4-gitpod/build-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGE_BASE_NAME="gitpod-simplism-playground"
-IMAGE_TAG="0.0.3"
+IMAGE_TAG="0.0.4"
 docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
 docker buildx build \
 --build-arg="GO_ARCH=amd64" \
@@ -12,7 +12,12 @@ docker buildx build \
 --build-arg="EXTISM_JS_VERSION=1.0.0-rc3" \
 --build-arg="EXTISM_JS_ARCH=x86_64" \
 --build-arg="EXTISM_JS_OS=linux" \
+--build-arg="BINARYEN_VERSION=version_116" \
+--build-arg="BINARYEN_ARCH=x86_64" \
+--build-arg="BINARYEN_OS=linux" \
 --build-arg="NODE_MAJOR=20" \
 --platform linux/amd64 \
 --push -t ${DOCKER_USER}/${IMAGE_BASE_NAME}:${IMAGE_TAG} .
 
+echo "to test it:"
+echo "docker run -it ${DOCKER_USER}/${IMAGE_BASE_NAME}:${IMAGE_TAG}"
