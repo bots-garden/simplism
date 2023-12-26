b7ce2d28a8546154867c17e4fb2a78f2f2a7f799
@k33g <ph.charriere@gmail.com>
2023-12-26 08:10:38 +0000

    🐳 Docker: update user(sudo) for docker dev environment

--- benchmarks/make-benchmarks.sh
+++ benchmarks/make-benchmarks.sh
@@ -27,7 +27,7 @@ cd ..
 # ----------------------------
 cd simple-go-plugin
 tinygo build -scheduler=none --no-debug -o simple.wasm -target wasi main.go
-go run ../../main.go listen simple.wasm say_hello --http-port 8083 &
+simplism listen simple.wasm say_hello --http-port 8083 &
 sleep 5
 hey -n 3000 -c 1000 -m GET http://0.0.0.0:8083 > ../simplism-go-report.txt
 pkill -f go
@@ -39,7 +39,7 @@ cd ..
 cd simple-rust-plugin
 cargo clean
 cargo build --release --target wasm32-wasi
-go run ../../main.go listen ./target/wasm32-wasi/release/simple_plugin.wasm hello --http-port 8084 &
+simplism listen ./target/wasm32-wasi/release/simple_plugin.wasm hello --http-port 8084 &
 sleep 5
 hey -n 3000 -c 1000 -m GET http://0.0.0.0:8084 > ../simplism-rust-report.txt
 pkill -f go
