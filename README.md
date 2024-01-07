156009068836d58f16f0ec46198a2eaadc8e16ae
@k33g <ph.charriere@gmail.com>
2024-01-07 08:17:15 +0000

    📦 simplism v0.1.1 🐋 [whale]

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 ```bash
 SIMPLISM_DISTRO="Linux_arm64" # 👀 https://github.com/bots-garden/simplism/releases
-VERSION="0.0.9"
+VERSION="0.1.1"
 wget https://github.com/bots-garden/simplism/releases/download/v${VERSION}/simplism_${SIMPLISM_DISTRO}.tar.gz -O simplism.tar.gz 
 tar -xf simplism.tar.gz -C /usr/bin
 rm simplism.tar.gz
--- create-release.sh
+++ create-release.sh
@@ -4,6 +4,20 @@ set -o allexport; source .release.env; set +o allexport
 # You need to create a .github.env file with these variables
 # - GITHUB_TOKEN
 
+: <<'COMMENT'
+Todo:
+- update of .release.env:
+  - TAG
+  - MESSAGE
+  - IMAGE_TAG
+  - GITPOD_IMAGE_TAG (only if needed)
+- update of README.md
+- update of generators/docker.image.txt
+- update of generators/version.txt
+- update of k8S/.env
+- update of k8S/README.md
+COMMENT
+
 echo "$TAG $MESSAGE"
 echo -n $MESSAGE > cmds/version.txt
 
