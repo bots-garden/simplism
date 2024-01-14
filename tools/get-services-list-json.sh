01b98e7070aaef1081d4ee465414fddd8268c19a
@k33g <ph.charriere@gmail.com>
2024-01-14 07:48:09 +0000

    ✨ Introduce new feature(s): Next Simplism CLI specifications

--- tools/get-services-list-json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o allexport; source .simplismconfig.env; set +o allexport
-
-curl ${DISCOVERY_SERVICE_URL} \
--H 'content-type:application/json; charset=UTF-8' \
--H "admin-discovery-token:${ADMIN_DISCOVERY_TOKEN}"
